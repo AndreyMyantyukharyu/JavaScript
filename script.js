7155cbcc30d900966ca9131adabdd73ac9097582
AndreyMyantyukharyu <andreym.up@gmail.com>
2020-09-17 18:07:08 +0300

    First practice

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 "use strict";
+const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');
 
-console.log('arr' + " - object" );
-// console.log(4 +  +"5" );
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-let incr = 10,
-    decr = 10;
-// ++incr;
-// --decr;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 !== '6');
-
-
-const isCheked = false,
-      isClose = false;
-console.log(isCheked || !isClose);
+console.log(personalMovieDB);
